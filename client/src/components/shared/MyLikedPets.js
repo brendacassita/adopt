071e7f96160ea93434f9d650c7afa0dba7ae0a48
Brenda <brendacassita@gmail.com>
2022-04-09 11:03:23 -0600

    added mylikedpets page

---
+++ client/src/components/shared/MyLikedPets.js
@@ -0,0 +1,14 @@
+import Card from "../Card"
+
+const MyLikedPets = ()=>{
+
+    return (
+        <Card>
+        <div>
+            <h1>MyLikedPets</h1>
+
+        </div>
+        </Card>
+    )
+} 
+export default MyLikedPets
