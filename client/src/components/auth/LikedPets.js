60e8cd10bdafa7d9822990e8b567dca007bbe970
Brenda <brendacassita@gmail.com>
2022-04-09 11:37:41 -0600

    liked pets page

--- client/src/components/auth/LikedPets.js
+++ client/src/components/auth/LikedPets.js
@@ -1,14 +1,39 @@
 import Card from "../Card"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 const LikedPets = ()=>{
+    const [pets, setPets] =  useState([])
+
+    useEffect(()=>{
+        getLikedPets()
+    }, [])
+
+const getLikedPets = async ()=>{
+    let res = await axios.get('/api/my_pets')
+    console.log(res)
+    setPets(res.data)
+}
+
 
     return (
         <Card>
-        <div>
+        <>
             <h1>Liked pets page</h1>
-            <p>Show liked pets</p>
-        </div>
+            {JSON.stringify}
+            {pets.map((pet)=>{
+                return(
+                    <div key = {pet.id}>
+                        <h2>{pet.name}</h2>
+                        <p>{pet.id}</p>
+                        </div>
+                )
+            })}
+        </>
         </Card>
     )
 } 
 export default LikedPets
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
+
+    get 'my_pets', to:'pets#my_pets'
     resources :pets do
+
+      
     end
   end
 end
